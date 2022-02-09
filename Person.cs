b835fe651fb878c4098397589bb1684c907a642a
PriyankaDhasade27 <98118311+PriyankaDhasade27@users.noreply.github.com>
2022-02-09 23:16:51 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Person
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                if(string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Name can't be empty");
+
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if(value>=18 && value<=65)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("please enter age between 18 and 65");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment_1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.Write("Enter your name here:");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter your age here");
+            person.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is {0} and age is{1}", person.Name, person.Age);
+
+        }
+    }
+}
+
+
+
+
+
+
+
